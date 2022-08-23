06015587db0b11bb490a4bb1818cf4c3409bca14
Gerry Lowe <gerard.lowe@stormid.com>
2022-08-23 13:32:39 +0100

    First cut

---
+++ JsonEverything/JsonValidator.cs
@@ -0,0 +1,21 @@
+﻿using Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace JsonEverything
+{
+    internal static class JsonValidator
+    {
+        public static void Validate(JsonSchema schema, string json)
+        {
+            var validationOptions = ValidationOptions.Default;
+            validationOptions.OutputFormat = OutputFormat.Basic;
+            var results = schema.Validate(JsonNode.Parse(json), validationOptions);
+            Console.WriteLine($"Validation result: {results.IsValid} {results.Message}");
+        }
+    }
+}
---
+++ JsonEverything/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Json.Schema;
+using JsonEverything;
+
+//
+// Some simple validation tests.
+//
+var mySchema = JsonSchema.FromText("""
+            {
+                "properties":{
+                "myProperty":{
+                    "type":"string",
+                    "minLength":10
+                }
+                },
+                "required":["myProperty"]
+            }
+            """);
+JsonValidator.Validate(mySchema, """
+{
+}
+""");
+JsonValidator.Validate(mySchema, """
+{
+    "myProperty":123
+}
+""");
+JsonValidator.Validate(mySchema, """
+{
+    "myProperty":"123456789"
+}
+""");
+JsonValidator.Validate(mySchema, """
+{
+    "myProperty":"1234567890"
+}
+""");
+
+//
+// Some simple merge tests.
+//
+JsonMerger.Merge("""
+{
+    "myProperty":"1234567890"
+}
+""", """
+{
+    "yourProperty":"hello"
+}
+""");
+
+Console.WriteLine("Success!");
